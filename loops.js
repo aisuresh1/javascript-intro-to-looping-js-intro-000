a063d14ad8975e4ec0997c846900979ecaf41ab6
Aiswariya Suresh <suresh.aiswariya912@gmail.com>
2020-01-23 11:53:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,12 +18,14 @@ function whileLoop(number){
 }
 
 /*
-function incrementVariable(){
-  i = i + 1;
-  return i;
+
 }
 */
 function doWhileLoop(number){
+  var i = 0
+  function incrementVariable(){
+    i = i + 1;
+    return i;
   do {
     console.log("I run once regardless.")
   } while{incrementVariable() < number}
